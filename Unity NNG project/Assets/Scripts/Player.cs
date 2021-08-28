8b540e992a9ea729a6bd86180aaba68f1156f8ba
King <aclengen@gmail.com>
2021-08-28 13:51:41 -0400

    Animations
    * Completed Blend Trees for Idle, Walk, Run
    * Transition *sort of* works between Run and Walk
    ** Need to switch to Rigidbody2D for movement manipulation to simulate slide

--- Unity NNG project/Assets/Scripts/Player.cs
+++ Unity NNG project/Assets/Scripts/Player.cs
@@ -12,9 +12,15 @@ public class Player : MonoBehaviour
     private float _currentPlayerHealth; //cache health globally
 
     [SerializeField]
-    private float _playerDefaultSpeed = 7f;
+    private float _playerMaxSpeed; //cache max speed
+    [SerializeField]
+    private float _playerDefaultSpeed = 7f; //set default speed
     [SerializeField]
     private float _currentPlayerSpeed; //cache speed globally
+    [SerializeField]
+    private float _playerAcceleration = 2f;
+    [SerializeField]
+    private float _playerDeceleration = 3f;
 
     Vector3 _playerDirection; //cache player direction globally
     Vector3 _playerPosition; //cache player position globally
@@ -39,7 +45,7 @@ public class Player : MonoBehaviour
 
     public void OnStart()
     {
-        _currentPlayerSpeed = _playerDefaultSpeed; //setting _currentPlayerSpeed to _playerDefaultSpeed
+        _playerMaxSpeed = _playerDefaultSpeed;
         _playerAnimation = GetComponent<Animator>(); //cache animator component
     }
 
@@ -48,18 +54,66 @@ public class Player : MonoBehaviour
         _playerInput = context.ReadValue<Vector2>(); //read input given
         _playerDirection = new Vector3(_playerInput.x, _playerInput.y, 0); //move in direction based on that input
     }
+
     public void OnMove()
     {
-        if (_playerInput != Vector2.zero) {
+
+        StartCoroutine(OnMoving());
+
+        if (_playerInput != Vector2.zero)
+        {
             transform.Translate(_playerDirection * _currentPlayerSpeed * Time.deltaTime); //move in direction based on direction and speed\
 
             _playerAnimation.SetFloat("Move X", _playerInput.x); //set blend tree Move X to calculate based on input on X axis
             _playerAnimation.SetFloat("Move Y", _playerInput.y); //the above but on the Y
 
-            _playerAnimation.SetBool("Walking", true); //set walking bool to true
-        } else
+            //_playerAnimation.SetBool("Walking", true); //set walking bool to true
+
+            if (_currentPlayerSpeed >= 3.5f)
+            {
+                _playerAnimation.SetBool("Running", true);
+                _playerAnimation.SetBool("Walking", false);
+            }
+
+            if (_currentPlayerSpeed <= 3.5f)
+            {
+                _playerAnimation.SetBool("Running", false);
+                _playerAnimation.SetBool("Walking", true);
+            }
+        }
+        else
         {
             _playerAnimation.SetBool("Walking", false);
+            _playerAnimation.SetBool("Running", false);
+        }
+    }
+
+    IEnumerator OnMoving()
+    {
+        if (_playerInput != Vector2.zero)
+        {
+            if (_currentPlayerSpeed < _playerMaxSpeed)
+            {
+                _currentPlayerSpeed += _playerAcceleration * Time.deltaTime;
+
+                if (_currentPlayerSpeed >= _playerMaxSpeed)
+                {
+                    _currentPlayerSpeed = _playerMaxSpeed;
+                }
+            }
+            yield return null; 
+        } else if (_playerInput == Vector2.zero)
+        {
+            if (_currentPlayerSpeed <= _playerMaxSpeed)
+            {
+                _currentPlayerSpeed -= _playerDeceleration * Time.deltaTime;
+
+                if (_currentPlayerSpeed <= 0f)
+                {
+                    _currentPlayerSpeed = 0f;
+                }
+            }
+            yield return null;
         }
     }
 
