05fbcd57c7bf288e195772f0e80bf5329e5109f7
King <aclengen@gmail.com>
2021-09-04 15:58:42 -0400

    Squashed commit of the following:
    commit 099b099846d477ac81883090d988b780849b0670
    Author: King <aclengen@gmail.com>
    Date:   Sat Sep 4 15:41:43 2021 -0400
    
        Deceleration control
    
        Check inspector
    
    commit fa2af3939e2d9d34a1c91c141968049552bd6220
    Author: King <aclengen@gmail.com>
    Date:   Sat Sep 4 10:02:20 2021 -0400
    
        Movement
    
        Movement working on a basic level, further testing needs to be done
        *Removed unneeded variables
        *Code comments made
    
    commit adcf814e6c1d9ad1f79b50800257cb4a97b1fbda
    Author: King <aclengen@gmail.com>
    Date:   Thu Sep 2 06:40:44 2021 -0400
    
        Gitignore Fix?
    
    commit 6ecc614ed10524b853ecf09bc896bf4448b2eb57
    Author: King <aclengen@gmail.com>
    Date:   Wed Sep 1 14:02:18 2021 -0400
    
        REDO MOVEMENT FUCK ADDFORCE
    
        we're gangster baby
    
    commit d931d53e94256d64924b5a889b7a987d8c9055f6
    Author: King <aclengen@gmail.com>
    Date:   Mon Aug 30 18:12:52 2021 -0400
    
        Small changes
    
        Must take break
    
    commit d8335b65f672723d84073d2c29b9daeebb1cf022
    Author: King <aclengen@gmail.com>
    Date:   Mon Aug 30 12:35:48 2021 -0400
    
        Update Player.cs
    
        Player has a little more control by capping max velocity, needs tuning because now that I have a turn check we can't hit over 2.9999f.
    
    commit 9259d57a145854356d562357cf35606aa3194b97
    Author: King <aclengen@gmail.com>
    Date:   Sun Aug 29 21:02:58 2021 -0400
    
        Refactoring
    
        Code clean-up of test code, will continue tomorrow
    
    commit f15ca9079b3c54ca69c528aca9056ea96d01edd9
    Author: King <aclengen@gmail.com>
    Date:   Sun Aug 29 15:44:37 2021 -0400
    
        Trying AddForce instead of Velocity
    
        This sucks weiners
    
    commit b4d287dbc02a98231720864e81b980fa9f1295d5
    Author: King <aclengen@gmail.com>
    Date:   Sat Aug 28 20:02:34 2021 -0400
    
        Start of Rigidbody2D Movement
    
        I am stuck, help god

--- Unity NNG project/Assets/Scripts/Player.cs
+++ Unity NNG project/Assets/Scripts/Player.cs
@@ -5,116 +5,101 @@ using UnityEngine.InputSystem; //use Unity New Input System
 
 public class Player : MonoBehaviour
 {
-    //Player Attributes
-    [SerializeField] //show variable below so that it can be edited
-    private float _playerHealth = 100f;
-    [SerializeField]
-    private float _currentPlayerHealth; //cache health globally
+    [Header("Player Movement")]
 
-    [SerializeField]
-    private float _playerMaxSpeed; //cache max speed
-    [SerializeField]
-    private float _playerDefaultSpeed = 7f; //set default speed
-    [SerializeField]
-    private float _currentPlayerSpeed; //cache speed globally
-    [SerializeField]
-    private float _playerAcceleration = 2f;
-    [SerializeField]
-    private float _playerDeceleration = 3f;
+    [SerializeField] private Vector3 velocity, desiredVelocity;
+    [SerializeField, Range(0f, 100f)] private float _maxVelocity = 10f; //speed cap
+    [SerializeField] private float _currentVelocity; //rate of velocity based on acceleration
+    [SerializeField, Range(0f, 100f)] private float _acceleration = 10f;
+    [SerializeField, Range(0f, 100f)] private float _deceleration = 10f;
 
-    Vector3 _playerDirection; //cache player direction globally
-    Vector3 _playerPosition; //cache player position globally
-    Vector2 _playerInput; //cache input globally
-
-    private Animator _playerAnimation; //cache animator component globally
-
+    [Space]
+    [Header("Player Coordinates")]
+    public Vector2 _playerInput; //cache input
+    public Vector3 _playerDirection; //cache player direction
+    [Space]
+    [Header("Player Components")]
+    private PlayerInput _playerControls; //cache playerinput
+    private Rigidbody2D _playerBody; //cache rigidbody
+    private Animator _playerAnimation; //cache animator
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        //smell my funky farts
-        OnStart();
+        _playerControls = GetComponent<PlayerInput>(); //cache playerinput component into variable
+        _playerAnimation = GetComponent<Animator>(); //cache animator component into variable
+        _playerBody = GetComponent<Rigidbody2D>(); //cache rigidbody2d component into variable
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         OnMove(); //call movement when input is called
     }
 
-    public void OnStart()
+    public void OnInput(InputAction.CallbackContext context)
     {
-        _playerMaxSpeed = _playerDefaultSpeed;
-        _playerAnimation = GetComponent<Animator>(); //cache animator component
-    }
+        _playerInput = context.ReadValue<Vector2>(); //read input value (Vector2)
+        _playerInput = Vector2.ClampMagnitude(_playerInput, 1f); //normalize input
+        _playerDirection = new Vector3(_playerInput.x, _playerInput.y, 0); //_playerInput in Vector3
 
-    public void OnDirection(InputAction.CallbackContext context)
-    {
-        _playerInput = context.ReadValue<Vector2>(); //read input given
-        _playerDirection = new Vector3(_playerInput.x, _playerInput.y, 0); //move in direction based on that input
+        desiredVelocity = (_playerDirection * _maxVelocity); //assign speed cap
     }
 
     public void OnMove()
     {
 
-        StartCoroutine(OnMoving());
+        velocity = _playerBody.velocity; //send rigidbody.velocity to velocity
+
+        //_currentVelocity = _acceleration * Time.deltaTime; //acceleration
+
+
+        if (_playerDirection != Vector3.zero)
+        {
+            _currentVelocity = _acceleration * Time.deltaTime; //acceleration
+        }
+        else if (_playerDirection == Vector3.zero)
+        {
+            _currentVelocity = _deceleration * Time.deltaTime; //deceleration
+        }
+
+
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, _currentVelocity); //move current velocity to desired velocity at accel rate (on x axis)
+        velocity.y = Mathf.MoveTowards(velocity.y, desiredVelocity.y, _currentVelocity); //move current velocity to desired velocity at accel rate (on y axis)
+
+        OnMoveAnimate(velocity.x, velocity.y); //animate based on velocity
+        _playerBody.velocity = velocity; //send velocity back to rigidbody.velocity
+
+    }
+
+    public void OnMoveAnimate(float _velocityX, float _velocityY) //z will be added eventually
+    {
+
+        _velocityX = Mathf.Abs(_velocityX); //returns absolute (-1 = 1) of velocity on x 
+        _velocityY = Mathf.Abs(_velocityY); //returns absolute (-1 = 1) of velocity on y
 
         if (_playerInput != Vector2.zero)
         {
-            transform.Translate(_playerDirection * _currentPlayerSpeed * Time.deltaTime); //move in direction based on direction and speed\
-
-            _playerAnimation.SetFloat("Move X", _playerInput.x); //set blend tree Move X to calculate based on input on X axis
-            _playerAnimation.SetFloat("Move Y", _playerInput.y); //the above but on the Y
-
-            //_playerAnimation.SetBool("Walking", true); //set walking bool to true
-
-            if (_currentPlayerSpeed >= 3.5f)
-            {
-                _playerAnimation.SetBool("Running", true);
-                _playerAnimation.SetBool("Walking", false);
-            }
-
-            if (_currentPlayerSpeed <= 3.5f)
-            {
-                _playerAnimation.SetBool("Running", false);
-                _playerAnimation.SetBool("Walking", true);
-            }
+            _playerAnimation.SetFloat("Move X", _playerDirection.x); //set blend tree Move X to calculate based on input on X axis
+            _playerAnimation.SetFloat("Move Y", _playerDirection.y); //the above but on the Y
         }
-        else
+
+
+        if (_velocityX >= 3.5f || _velocityY >= 3.5f) //if velocity is greater than or equal to 3.5
+        {
+            _playerAnimation.SetBool("Running", true);
+            _playerAnimation.SetBool("Walking", false);
+        }
+        if (_velocityX < 3.5f && _velocityX > 0 || _velocityY < 3.5f && _velocityY > 0) //if velocity is less than 3.5 and greater than 0
+        {
+            _playerAnimation.SetBool("Running", false);
+            _playerAnimation.SetBool("Walking", true);
+        }
+        if (_velocityX == 0 && _velocityY == 0) //if velocity is less than or equal to 0
         {
             _playerAnimation.SetBool("Walking", false);
             _playerAnimation.SetBool("Running", false);
         }
     }
-
-    IEnumerator OnMoving()
-    {
-        if (_playerInput != Vector2.zero)
-        {
-            if (_currentPlayerSpeed < _playerMaxSpeed)
-            {
-                _currentPlayerSpeed += _playerAcceleration * Time.deltaTime;
-
-                if (_currentPlayerSpeed >= _playerMaxSpeed)
-                {
-                    _currentPlayerSpeed = _playerMaxSpeed;
-                }
-            }
-            yield return null; 
-        } else if (_playerInput == Vector2.zero)
-        {
-            if (_currentPlayerSpeed <= _playerMaxSpeed)
-            {
-                _currentPlayerSpeed -= _playerDeceleration * Time.deltaTime;
-
-                if (_currentPlayerSpeed <= 0f)
-                {
-                    _currentPlayerSpeed = 0f;
-                }
-            }
-            yield return null;
-        }
-    }
-
 }
