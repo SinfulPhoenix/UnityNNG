c3b8032b3a8cfc28f0f187328827d28e27fe71d7
King <aclengen@gmail.com>
2021-09-07 12:17:47 -0400

    Squashed commit of the following:
    commit 6a2b98456f2cc8600a3caadd95948d2883028887
    Author: King <aclengen@gmail.com>
    Date:   Tue Sep 7 12:17:13 2021 -0400
    
        Direction Change
    
        * Tuned direction change code so that it properly registers change in direction and properly adds traction, as well as reduced the size of the code significantly
        * Refactored some elements and cleaned up overall code
        * To avoid sitting on negative numbers I have a velocity check to set the `rigidbody.velocity to 0` when not moving but only when an input isn't being pressed to avoid getting locked
    
    commit c4d0a9872f90047603026178f285550b3ecc5c82
    Author: King <aclengen@gmail.com>
    Date:   Mon Sep 6 18:52:30 2021 -0400
    
        Update Player.cs
    
    commit 7def4d49018b426f087f85285a07811d96f36a15
    Author: King <aclengen@gmail.com>
    Date:   Sun Sep 5 18:48:55 2021 -0400
    
        Update Player.cs
    
    commit f99c1888959480c781d43148c0009204bbc3e08d
    Author: King <aclengen@gmail.com>
    Date:   Sun Sep 5 14:07:41 2021 -0400
    
        Modularity Prep
    
    commit 075172289d35483f1f5e05d2c656a77b6608e20f
    Author: King <aclengen@gmail.com>
    Date:   Sun Sep 5 09:49:49 2021 -0400
    
        Update PackageManagerSettings.asset
    
    commit 6a2b976653ba21cf19d23715c06b807509946149
    Author: King <aclengen@gmail.com>
    Date:   Sat Sep 4 21:22:26 2021 -0400
    
        Update Player Animations.controller
    
    commit d4874e295424fcd330eba49fa903e6af985b8ef2
    Author: King <aclengen@gmail.com>
    Date:   Sat Sep 4 21:22:17 2021 -0400
    
        Update Player.cs
    
    commit b985a88e0a707a70fd892acfc4cb1c0c2c3cc949
    Author: King <aclengen@gmail.com>
    Date:   Sat Sep 4 19:30:12 2021 -0400
    
        Traction
    
        Added traction
        * Can be tweaked via the modifiers _traction and _tractionTimer, both commented
    
    commit 606c24a2f83305625074bc8ff459d524867a960f
    Author: King <aclengen@gmail.com>
    Date:   Sat Sep 4 15:41:43 2021 -0400
    
        Deceleration control
    
        Check inspector
    
    commit 8219dc20b99aaa8ed5d57b3ab2c1d12434c264e7
    Author: King <aclengen@gmail.com>
    Date:   Sat Sep 4 10:02:20 2021 -0400
    
        Movement
    
        Movement working on a basic level, further testing needs to be done
        *Removed unneeded variables
        *Code comments made
    
    commit 3e7c1192c69e7fa6fa3dc9d5dc065c659fa8a84e
    Author: King <aclengen@gmail.com>
    Date:   Thu Sep 2 06:40:44 2021 -0400
    
        Gitignore Fix?
    
    commit a47a61506b5b09ca83091932a7107a153ab2315d
    Author: King <aclengen@gmail.com>
    Date:   Wed Sep 1 14:02:18 2021 -0400
    
        REDO MOVEMENT FUCK ADDFORCE
    
        we're gangster baby
    
    commit 1025cf72e88b9fcb6e0718dd7f761b2ae0c797c1
    Author: King <aclengen@gmail.com>
    Date:   Mon Aug 30 18:12:52 2021 -0400
    
        Small changes
    
        Must take break
    
    commit 8fef0170564306e3b3a833b19ea98dbf72412e36
    Author: King <aclengen@gmail.com>
    Date:   Mon Aug 30 12:35:48 2021 -0400
    
        Update Player.cs
    
        Player has a little more control by capping max velocity, needs tuning because now that I have a turn check we can't hit over 2.9999f.
    
    commit 7fb45c03471ae1204dc06fa7053c3644df275e72
    Author: King <aclengen@gmail.com>
    Date:   Sun Aug 29 21:02:58 2021 -0400
    
        Refactoring
    
        Code clean-up of test code, will continue tomorrow
    
    commit a08ec8fda3f2cdfafc9bae595c3ca62401a02999
    Author: King <aclengen@gmail.com>
    Date:   Sun Aug 29 15:44:37 2021 -0400
    
        Trying AddForce instead of Velocity
    
        This sucks weiners
    
    commit f5e4a0c9b9f6cb8b3cc4979a7733ac22560f44ce
    Author: King <aclengen@gmail.com>
    Date:   Sat Aug 28 20:02:34 2021 -0400
    
        Start of Rigidbody2D Movement
    
        I am stuck, help god

--- Unity NNG project/Assets/Scripts/Player.cs
+++ Unity NNG project/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,31 +8,40 @@ public class Player : MonoBehaviour
 {
     [Header("Player Movement")]
 
-    [SerializeField] private Vector3 velocity, desiredVelocity;
     [SerializeField, Range(0f, 100f)] private float _maxVelocity = 10f; //speed cap
-    [SerializeField] private float _currentVelocity; //rate of velocity based on acceleration
+    [SerializeField] private float _velocity; //rate of velocity based on acceleration
+    [SerializeField] private Vector3 _currentVelocity, _desiredVelocity;
     [SerializeField, Range(0f, 100f)] private float _acceleration = 10f;
     [SerializeField, Range(0f, 100f)] private float _deceleration = 20f;
 
+    [SerializeField] private float _currentTraction; 
+    [SerializeField, Range(0f, 50f)] private float _traction = 4f; //how much drag/traction to stop sliding
+    [SerializeField, Range(0f, 1f)] private float _tractionTimer = 0.35f; //drag/traction timer: lower = less time to control slide
+
     [Space]
     [Header("Player Coordinates")]
-    public Vector2 _playerInput; //cache input
-    public Vector3 _playerDirection; //cache player direction
+
+    [SerializeField] private Vector2 _currentInput; //cache current input
+    [SerializeField] private Vector3 _currentDirection; //cache vector3 convert of vector2 input
+    [SerializeField] private Vector3 _lastDirection; //cache last input to compare
+    private bool _directionChange => (_currentDirection != _lastDirection); //trigger for stopping sliding
+
     [Space]
     [Header("Player Components")]
-    private PlayerInput _playerControls; //cache playerinput
-    private Rigidbody2D _playerBody; //cache rigidbody
-    private Animator _playerAnimation; //cache animator
+    //private PlayerInput _controls; //cache playerinput
+    private Rigidbody2D _body; //cache rigidbody
+    private Animator _animation; //cache animator
 
 
     // Start is called before the first frame update
     private void Awake()
     {
-        _playerControls = GetComponent<PlayerInput>(); //cache playerinput component into variable
-        _playerAnimation = GetComponent<Animator>(); //cache animator component into variable
-        _playerBody = GetComponent<Rigidbody2D>(); //cache rigidbody2d component into variable
-    }
+        //_controls = GetComponent<PlayerInput>(); //cache playerinput component into variable
+        _animation = GetComponent<Animator>(); //cache animator component into variable
+        _body = GetComponent<Rigidbody2D>(); //cache rigidbody2d component into variable
 
+    }
+    
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -40,37 +50,67 @@ public class Player : MonoBehaviour
 
     public void OnInput(InputAction.CallbackContext context)
     {
-        _playerInput = context.ReadValue<Vector2>(); //read input value (Vector2)
-        _playerInput = Vector2.ClampMagnitude(_playerInput, 1f); //normalize input
-        _playerDirection = new Vector3(_playerInput.x, _playerInput.y, 0); //_playerInput in Vector3
 
-        desiredVelocity = (_playerDirection * _maxVelocity); //assign speed cap
+        _currentInput = context.ReadValue<Vector2>(); //read input value (Vector2)
+        _currentInput = Vector2.ClampMagnitude(_currentInput, 1f); //normalize input
+        _currentDirection = new Vector3(_currentInput.x, _currentInput.y, 0); //_currentInput in Vector3
+
+        StartCoroutine(PreviousInputRecord(_tractionTimer));
+
     }
 
     public void OnMove()
     {
+        _desiredVelocity = (_currentDirection * _maxVelocity); //assign speed cap
+        _currentVelocity = _body.velocity; //send rigidbody.velocity to velocity
+        _currentTraction = _body.drag; //traction is linear drag, wow
 
-        velocity = _playerBody.velocity; //send rigidbody.velocity to velocity
 
-        //_currentVelocity = _acceleration * Time.deltaTime; //acceleration
+        ApplyPhysics();
 
+        //move current velocity to desired velocity at accel rate (on x axis)
+        _currentVelocity.x = Mathf.MoveTowards(_currentVelocity.x, _desiredVelocity.x, _velocity);
+        //move current velocity to desired velocity at accel rate (on y axis)
+        _currentVelocity.y = Mathf.MoveTowards(_currentVelocity.y, _desiredVelocity.y, _velocity); 
 
-        if (_playerDirection != Vector3.zero)
-        {
-            _currentVelocity = _acceleration * Time.deltaTime; //acceleration
+        if (_velocity == 0f){ //if we're not moving
+            _currentVelocity = Vector3.zero; //zero out rigidbody.velocity to avoid negative numbers
         }
-        else if (_playerDirection == Vector3.zero)
+
+        OnMoveAnimate(_currentVelocity.x, _currentVelocity.y); //animate based on velocity
+        _body.velocity = _currentVelocity; //send velocity back to rigidbody.velocity
+        _body.drag = _currentTraction;
+
+    }
+
+    IEnumerator PreviousInputRecord(float _tractionTimer)
+    {
+        yield return new WaitForSeconds(_tractionTimer);
+        _lastDirection = new Vector3(_currentDirection.x, _currentDirection.y, 0);
+    }
+
+    void ApplyPhysics()
+    {
+        if (_currentDirection != Vector3.zero) //if there are inputs
         {
-            _currentVelocity = _deceleration * Time.deltaTime; //deceleration
+            _velocity = _acceleration * Time.deltaTime; //accelerate
+
+            if (_directionChange) //control slide when changing direction
+            {
+                _currentTraction = _traction; //by using traction
+            } else { _currentTraction = 0f; } 
+        }
+        else if (_currentDirection == Vector3.zero) //&& _currentTraction == 0f) //if there arent inputs
+        {
+            _velocity = _deceleration * Time.deltaTime; //decelerate
+            
+            if (_currentVelocity == Vector3.zero)
+            {
+                _velocity = 0f; //if no input, _velocity = 0
+            }
         }
 
-
-        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, _currentVelocity); //move current velocity to desired velocity at accel rate (on x axis)
-        velocity.y = Mathf.MoveTowards(velocity.y, desiredVelocity.y, _currentVelocity); //move current velocity to desired velocity at accel rate (on y axis)
-
-        OnMoveAnimate(velocity.x, velocity.y); //animate based on velocity
-        _playerBody.velocity = velocity; //send velocity back to rigidbody.velocity
-
+        
     }
 
     public void OnMoveAnimate(float _velocityX, float _velocityY) //z will be added eventually
@@ -79,27 +119,27 @@ public class Player : MonoBehaviour
         _velocityX = Mathf.Abs(_velocityX); //returns absolute (-1 = 1) of velocity on x 
         _velocityY = Mathf.Abs(_velocityY); //returns absolute (-1 = 1) of velocity on y
 
-        if (_playerInput != Vector2.zero)
+        if (_currentDirection != Vector3.zero)
         {
-            _playerAnimation.SetFloat("Move X", _playerDirection.x); //set blend tree Move X to calculate based on input on X axis
-            _playerAnimation.SetFloat("Move Y", _playerDirection.y); //the above but on the Y
+            _animation.SetFloat("Move X", _currentDirection.x); //set blend tree Move X to calculate based on input on X axis
+            _animation.SetFloat("Move Y", _currentDirection.y); //the above but on the Y
         }
 
 
         if (_velocityX >= 3.5f || _velocityY >= 3.5f) //if velocity is greater than or equal to 3.5
         {
-            _playerAnimation.SetBool("Running", true);
-            _playerAnimation.SetBool("Walking", false);
+            _animation.SetBool("Running", true);
+            _animation.SetBool("Walking", false);
         }
         if (_velocityX < 3.5f && _velocityX > 0 || _velocityY < 3.5f && _velocityY > 0) //if velocity is less than 3.5 and greater than 0
         {
-            _playerAnimation.SetBool("Running", false);
-            _playerAnimation.SetBool("Walking", true);
+            _animation.SetBool("Running", false);
+            _animation.SetBool("Walking", true);
         }
         if (_velocityX == 0 && _velocityY == 0) //if velocity is less than or equal to 0
         {
-            _playerAnimation.SetBool("Walking", false);
-            _playerAnimation.SetBool("Running", false);
+            _animation.SetBool("Walking", false);
+            _animation.SetBool("Running", false);
         }
     }
 }
